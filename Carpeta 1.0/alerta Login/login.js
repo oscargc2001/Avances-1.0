797035a42126f4f583e0b74752a778cfe096ddf5
oscargc2001 <programasogc2001@gmail.com>
2024-07-17 16:48:30 -0600

    Create login.js

---
+++ Carpeta 1.0/alerta Login/login.js
@@ -0,0 +1,9 @@
+
+document.addEventListener('DOMContentLoaded', ()=>{
+    //Creamos la funcion de la alerta 
+    setTimeout( () => {
+        document.getElementById('alerta').style.display = 'none';
+    }, 3000);
+
+})
+  
