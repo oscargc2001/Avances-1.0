299416e0a4b5579fe4ee1bbf588abd73d8800b8a
oscargc2001 <programasogc2001@gmail.com>
2024-07-17 16:42:18 -0600

    Create header.php

---
+++ Carpeta 1.0/view/MainHeader/header.php
@@ -0,0 +1,51 @@
+<header class="site-header">
+	    <div class="container-fluid">
+	
+	        <a href="#" class="site-logo">
+	            <img class="hidden-md-down" src="../../public/img/logo-2.png" alt="">
+	            <img class="hidden-lg-up" src="../../public/img/logo-2-mob.png" alt="">
+	        </a>
+	
+	        <button id="show-hide-sidebar-toggle" class="show-hide-sidebar">
+	            <span>toggle menu</span>
+	        </button>
+	
+	        <button class="hamburger hamburger--htla">
+	            <span>toggle menu</span>
+	        </button>
+
+	        <div class="site-header-content">
+	            <div class="site-header-content-in">
+                <div class="site-header-shown">
+
+	                    <div class="dropdown user-menu">
+	                        <button class="dropdown-toggle" id="dd-user-menu" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+	                            <img src="../../public/img/avatar-2-64.png" alt="">
+	                        </button>
+	                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dd-user-menu">
+	                            <a class="dropdown-item" href="#"><span class="font-icon glyphicon glyphicon-user"></span>Perfil</a>
+	                            <a class="dropdown-item" href="#"><span class="font-icon glyphicon glyphicon-question-sign"></span>Ayuda</a>
+	                            <div class="dropdown-divider"></div>
+	                            <a class="dropdown-item" href="../Salir/salir.php"><span class="font-icon glyphicon glyphicon-log-out"></span>Salir</a>
+	                        </div>
+	                    </div>
+	
+	                 
+	                </div><!--.site-header-shown-->
+
+					<input type="hidden" id="user_idx" value="<?php $_SESSION["id_usuario"] ?>">
+	
+	                <div class="mobile-menu-right-overlay"></div>
+					<div class="dropdown dropdown-typical">
+						<a href="#" class="dropdown-toggle no-arr"></a>
+						<span class="font-icon font-icon-user"></span>
+						<span class="lblcontactonomx"> <?php echo $_SESSION["usuario_nombre"] ?> <?php echo $_SESSION["usuario_apellido"] ?> </span>
+
+					</div>
+
+					
+	            
+	            </div><!--site-header-content-in-->
+	        </div><!--.site-header-content-->
+	    </div><!--.container-fluid-->
+	</header><!--.site-header-->
